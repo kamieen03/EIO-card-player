9772711162e7f4c54c03b3d03ac152b8371cd447
filip <filip.bonczyk@student.put.poznan.pl>
2019-10-28 23:00:39 +0100

    CruelPlayer v2

--- players/CruelPlayer.py
+++ players/CruelPlayer.py
@@ -16,6 +16,7 @@ class CruelPlayer(Player):
         super().__init__(name)
         self.overview = None
         self.pile = []
+        self.opponent_draw = True
 
     def _init_overview(self):
         overview = {(value, color): Owner.IDK for value in range(9, 15) for color in range(4)}
@@ -36,6 +37,8 @@ class CruelPlayer(Player):
 
         ### check if must draw
         if len(self.cards) == 1 and declared_card is not None and self.cards[0][0] < declared_card[0]:
+            if len(self.pile) > 1:
+                self.pile.pop()
             return "draw"
 
         card = min(self.cards, key=lambda x: x[0])
@@ -53,6 +56,7 @@ class CruelPlayer(Player):
         #print(len([c for c, o in self.overview.items() if o == Owner.BOOT]))
         #print([c for c, o in self.overview.items() if o == Owner.IDK])
         #print("------")
+        self.opponent_draw = False
         if opponent_declaration in self.cards:
             return True
         elif opponent_declaration in [c for c, o in self.overview.items() if o == Owner.BOOT]:
@@ -61,6 +65,8 @@ class CruelPlayer(Player):
             return np.random.choice([True, False], p=[0.3, 0.7])
 
     def getCheckFeedback(self, checked, iChecked, iDrewCards, revealedCard, noTakenCards, log=True):
+        print(self.pile)
+        print(noTakenCards)
         if checked:
             if iChecked:
                 if not iDrewCards:
@@ -74,6 +80,19 @@ class CruelPlayer(Player):
                         self.overview[self.pile.pop()] = Owner.BOOT
                     if noTakenCards > 2:
                         self.overview[self.pile.pop()] = Owner.BOOT
+        elif self.opponent_draw:
+            print("Draw")
+            if len(self.pile) > 0:
+                self.overview[self.pile.pop()] = Owner.BOOT
+            if len(self.pile) > 2:
+                self.overview[self.pile.pop()] = Owner.BOOT
+
+        self.opponent_draw = True
+
+    def pop_n(self, n):
+        for i in range(n):
+            if self.pile[-1]:
+                self.pile.pop()
 
     def startGame(self, cards):
         super().startGame(cards)
